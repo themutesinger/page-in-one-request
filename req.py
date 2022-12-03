460a1086db10d1627d51bb3e816b320bcb8f2d96
Suvan <overmind77s@gmail.com>
2022-12-03 18:18:04 +0600

    finished

---
+++ req.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import webbrowser
+
+import requests
+from fake_headers import Headers
+
+
+def main():
+    argsparser = argparse.ArgumentParser(description='Requests')
+    argsparser.add_argument('-o', help='open in chrome')
+    arguments = argsparser.parse_known_args()
+
+    if arguments[0].o != None:
+        response = requests.get(url=arguments[0].o)
+
+        with open('index.html', 'w') as file:
+            file.write(response.text)
+
+        chrome_path = '/usr/bin/google-chrome-stable %s'
+        file_pash = os.path.abspath('index.html')
+        webbrowser.get(chrome_path).open(file_pash)
+
+    if len(arguments[1]) != 0:
+        response = requests.get(url=arguments[1][0])
+        print(response.text)
+
+    print(arguments)
+
+
+if __name__ == '__main__':
+    main()
